47fe91b6be63556c0aa46f8064ac89896b94e884
LukmanE22 <unsurlukman@gmail.com>
2023-01-22 11:32:24 +0700

    edit example page & homemodulecss

--- src/pages/example/index.tsx
+++ src/pages/example/index.tsx
@@ -15,17 +15,6 @@ export default function Home() {
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            <code className={styles.code}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, laudantium animi? Laudantium quam, magnam ipsam obcaecati vel natus provident quaerat eaque. Deserunt quis provident dolores libero
-              aliquid, tempore id illum earum delectus! Deleniti numquam fugit eveniet sequi quas quod voluptatum et, quos dicta. Ipsam perspiciatis in maxime. Repellendus, mollitia laudantium, quam vitae
-              debitis suscipit quod earum quasi ullam tempora adipisci officia unde voluptas voluptate magnam dolor accusamus nemo minima assumenda amet libero totam laboriosam. Ab eligendi officiis voluptates
-              delectus eaque iste, eveniet iure qui minus sed officia totam expedita molestias libero dicta ut aliquid voluptatem. Iusto deserunt provident earum animi?
-            </code>
-          </p>
-        </div>
-
         <div className={styles.grid}>
           <a href='https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app' className={styles.card} target='_blank' rel='noopener noreferrer'>
             <h2 className={inter.className}>
@@ -59,6 +48,17 @@ export default function Home() {
             </h2>
             <p className={inter.className}>Instantly deploy your Next.js site to a shareable URL with&nbsp;Vercel.</p>
           </a>
+        </div>
+
+        <div className={styles.description}>
+          <p>
+            <code className={(styles.code, styles.justify)}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, laudantium animi? Laudantium quam, magnam ipsam obcaecati vel natus provident quaerat eaque. Deserunt quis provident dolores libero
+              aliquid, tempore id illum earum delectus! Deleniti numquam fugit eveniet sequi quas quod voluptatum et, quos dicta. Ipsam perspiciatis in maxime. Repellendus, mollitia laudantium, quam vitae
+              debitis suscipit quod earum quasi ullam tempora adipisci officia unde voluptas voluptate magnam dolor accusamus nemo minima assumenda amet libero totam laboriosam. Ab eligendi officiis voluptates
+              delectus eaque iste, eveniet iure qui minus sed officia totam expedita molestias libero dicta ut aliquid voluptatem. Iusto deserunt provident earum animi?
+            </code>
+          </p>
         </div>
       </main>
     </>
